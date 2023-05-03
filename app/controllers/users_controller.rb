30d602b8b99ad416c44465de8d09cad3260a5691
Manish Roy <manishroy0112@gmail.com>
2023-05-03 20:45:13 +0530

    controllers updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
   def create
-    user = User.new(user_params)
-
-    if user.save
-      render json: {msg: "User Created"}
+    @user = User.new(user_params)
+    if @user.save
+      reset_session
+      log_in @user
+      flash[:success] = "Welcome"
+      redirect_to @user
+    else
+      render 'new', status: :unprocessable_entity
     end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
